a15a7e2d4808f6969d5f0032a58e62b94e7be144
Christellec64 <christelle.cwcs64@gmail.com>
2020-04-29 21:46:52 +0200

    adding checkbox for display on continent

--- src/components/worldPage/WorldPageBackGround.jsx
+++ src/components/worldPage/WorldPageBackGround.jsx
@@ -11,41 +11,55 @@ class WorldPageBackGround extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display : false,
+      continents: false, 
+      climat: false, 
+      population: false,
     };
+    this.handleOnClickCont = this.handleOnClickCont.bind(this);
+    this.handleOnClickClim = this.handleOnClickClim.bind(this);
+    this.handleOnClickPop = this.handleOnClickPop.bind(this);
   }
   
+  handleOnClickCont() {
+    this.setState({ continents: !this.state.continents });
+  }
+  handleOnClickClim() {
+    this.setState({ climat: !this.state.climat });
+  }
+  handleOnClickPop() {
+    this.setState({ population: !this.state.population });
+  }
   render() {
     return (
       <Container style={{ width: "100%", padding: 0 }}>
         <Row style={{ width: "100%" }}>
           <Col xs="4">
             <img src={NA} alt="Amérique du Nord" class="rounded" width="100%" />
-            <p>Amérique du Nord</p>
+            <p className={this.state.continents ? "displayOn": "displayOff"}>Amérique du Nord</p>
           </Col>
           <Col xs="4">
             <img src={EU} alt="Europe" class="rounded" width="100%" />
-            <p>Europe</p>
+            <p className={this.state.continents ? "displayOn": "displayOff"}>Europe</p>
           </Col>
           <Col xs="4" className="text-left">
             <img src={Asia} alt="Europe" class="rounded" width="100%" />
-            <p>Asie</p>
+            <p className={this.state.continents ? "displayOn": "displayOff"}>Asie</p>
           </Col>
         </Row>
         <Row>
           <Col xs="4" className="text-right">
             <img src={SA} alt="Europe" class="rounded" height="300" />
-            <p>Amérique du Sud</p>
+            <p className={this.state.continents ? "displayOn": "displayOff"}>Amérique du Sud</p>
           </Col>
           <Col xs="5" className="text-left">
             {" "}
             <img src={Africa} alt="Europe" class="rounded" height="300" />
-            <p>Afrique</p>
+            <p className={this.state.continents ? "displayOn": "displayOff"}>Afrique</p>
           </Col>
           <Col xs="3">
             {" "}
             <img src={Oceania} alt="Europe" class="rounded" height="300" />
-            <p>Océanie</p>
+            <p className={this.state.continents ? "displayOn": "displayOff"}>Océanie</p>
           </Col>
         </Row>
       </Container>
