f99e6ccc0d2b1d3c011b52f0dda6582809006c68
Christellec64 <christelle.cwcs64@gmail.com>
2020-04-29 21:35:02 +0200

    adding paragraph

--- src/components/worldPage/WorldPageBackGround.jsx
+++ src/components/worldPage/WorldPageBackGround.jsx
@@ -11,64 +11,41 @@ class WorldPageBackGround extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: "",
+      display : false,
     };
   }
+  
   render() {
     return (
-      <Container style={{width:"100%", padding:0}}>
-        <Row style={{width:"100%"}}>
+      <Container style={{ width: "100%", padding: 0 }}>
+        <Row style={{ width: "100%" }}>
           <Col xs="4">
-            <img
-              src={NA}
-              alt="Amérique du Nord"
-              class="rounded"
-              width="100%"
-            />
+            <img src={NA} alt="Amérique du Nord" class="rounded" width="100%" />
+            <p>Amérique du Nord</p>
           </Col>
           <Col xs="4">
-            <img
-              src={EU}
-              alt="Europe"
-              class="rounded"
-              width="100%"
-            />
+            <img src={EU} alt="Europe" class="rounded" width="100%" />
+            <p>Europe</p>
           </Col>
           <Col xs="4" className="text-left">
-            <img
-              src={Asia}
-              alt="Europe"
-              class="rounded"
-              width="100%"
-            />
+            <img src={Asia} alt="Europe" class="rounded" width="100%" />
+            <p>Asie</p>
           </Col>
         </Row>
         <Row>
           <Col xs="4" className="text-right">
-            <img
-              src={SA}
-              alt="Europe"
-              class="rounded"
-              height="300"
-            />
+            <img src={SA} alt="Europe" class="rounded" height="300" />
+            <p>Amérique du Sud</p>
           </Col>
           <Col xs="5" className="text-left">
             {" "}
-            <img
-              src={Africa}
-              alt="Europe"
-              class="rounded"
-              height="300"
-            />
+            <img src={Africa} alt="Europe" class="rounded" height="300" />
+            <p>Afrique</p>
           </Col>
           <Col xs="3">
             {" "}
-            <img
-              src={Oceania}
-              alt="Europe"
-              class="rounded"
-              height="300"
-            />
+            <img src={Oceania} alt="Europe" class="rounded" height="300" />
+            <p>Océanie</p>
           </Col>
         </Row>
       </Container>
