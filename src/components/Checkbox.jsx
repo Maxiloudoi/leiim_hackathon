09cd2e8e2c4d252d791bdc09ff7d932c95b87fce
Lio-code <lio4455@hotmail.com>
2020-04-29 17:50:48 +0200

    create 3 button

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Checkbox.module.css";
 
 class Checkbox extends React.Component {
   constructor(props) {
@@ -8,7 +9,22 @@ class Checkbox extends React.Component {
   }
 
   render() {
-    return <div>Mycheckbox</div>;
+    return (
+      <div className={styles.flexcol}>
+        <div className={styles.flexrow}>
+          <input type="checkbox" />
+          <p className={styles.textmargin}>Continent</p>
+        </div>
+        <div className={styles.flexrow}>
+          <input type="checkbox" />
+          <p className={styles.textmargin}>Climat</p>
+        </div>
+        <div className={styles.flexrow}>
+          <input type="checkbox" />
+          <p className={styles.textmargin}>Population</p>
+        </div>
+      </div>
+    );
   }
 }
 
